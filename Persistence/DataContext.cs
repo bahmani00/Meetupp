1a1c08779838b063a11d5c77ade69525c4ef3230
Daniel <bahmani00@gmail.com>
2020-11-22 13:44:17 -0500

    drafting the Walking Skeleton

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options): base(options){
+
+        }
+
+        public DbSet<Value> Values {get;set;}
+    }
+}
+
