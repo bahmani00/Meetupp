81bf6ccbc85e3aec415aa594d313a22c8b5b87b0
Daniel <bahmani00@gmail.com>
2020-12-19 22:10:51 -0500

    Add delay to endpoints(simulate real word)

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,13 +3,18 @@ import axios, { AxiosResponse } from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+const sleep = (ms: number) => 
+    (response: AxiosResponse) =>
+        new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+const sleepInMs = 500;        
+
 const responseBody = (response: AxiosResponse) => response.data;
 
 const requests = {
-    get:  (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put:  (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    del:  (url: string) => axios.delete(url).then(responseBody),
+    get:  (url: string) => axios.get(url).then(sleep(sleepInMs)).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(sleep(sleepInMs)).then(responseBody),
+    put:  (url: string, body: {}) => axios.put(url, body).then(sleep(sleepInMs)).then(responseBody),
+    del:  (url: string) => axios.delete(url).then(sleep(sleepInMs)).then(responseBody),
 };
 
 const Activities = {
@@ -20,4 +25,5 @@ const Activities = {
     delete:  (id: string) => requests.del(`/activity/${id}`)
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { Activities }
