51dc7ddacff1e444b5f390c4bb55f9d579cfb786
Daniel <bahmani00@gmail.com>
2020-12-25 11:04:42 -0500

    handle page NotFound & invalid Guid

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,14 +1,31 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
+import { history } from '../..';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-const sleep = (ms: number) => 
-    (response: AxiosResponse) =>
-        new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
-const sleepInMs = 500;        
+axios.interceptors.response.use(undefined, error => {
+    if (error.message === 'Network Error' && !error.response) {
+        toast.error('Network error - make sure API is running!')
+    }
+    const {status, data, config} = error.response;
+    if (status === 404) {
+        history.push('/notfound')
+    }
+    if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+        history.push('/notfound')
+    }
+    if (status === 500) {
+        toast.error('Server error - check the terminal for more info!')
+    }
+})
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+const sleep = (ms: number) => (response: AxiosResponse) => 
+    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+const sleepInMs = 500;        
 
 const requests = {
     get:  (url: string) => axios.get(url).then(sleep(sleepInMs)).then(responseBody),
