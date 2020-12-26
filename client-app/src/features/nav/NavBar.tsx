7aa6bb4559ca9647618ad8ef004c90af680cfa0f
Daniel <bahmani00@gmail.com>
2020-12-26 16:31:34 -0500

    - save token in storage - added logout func - added user menue

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -6,7 +6,7 @@ import { RootStoreContext } from '../../app/stores/rootStore';
 
 const NavBar: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
-  const { user } = rootStore.userStore;
+  const { user, logout } = rootStore.userStore;
   return (
     <Menu fixed='top' inverted>
       <Container>
@@ -34,7 +34,7 @@ const NavBar: React.FC = () => {
                   text='My profile'
                   icon='user'
                 />
-                <Dropdown.Item text='Logout' icon='power' />
+                <Dropdown.Item onClick={logout} text='Logout' icon='power' />
               </Dropdown.Menu>
             </Dropdown>
           </Menu.Item>
