9d144cd458390a336f36f96a4b493857c62984ef
Daniel <bahmani00@gmail.com>
2020-12-26 20:15:14 -0500

    -added register form - show errors separately

--- client-app/src/app/common/form/ErrorMessage.tsx
+++ client-app/src/app/common/form/ErrorMessage.tsx
@@ -11,6 +11,13 @@ const ErrorMessage: React.FC<IProps> = ({ error, text }) => {
   return (
     <Message error>
       <Message.Header>{error.statusText}</Message.Header>
+      {error.data && Object.keys(error.data.errors).length > 0 && (
+        <Message.List>
+          {Object.values(error.data.errors).flat().map((err, i) => (
+            <Message.Item key={i}>{err}</Message.Item>
+          ))}
+        </Message.List>
+      )}
       {text && <Message.Content content={text} />}
     </Message>
   );
