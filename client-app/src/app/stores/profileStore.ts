fdce7d806a5da999f798c71fda66dd5c4c0390c3
Daniel <bahmani00@gmail.com>
2020-12-30 15:16:01 -0500

    added missing import IUserActivity

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,6 +1,6 @@
 import {RootStore} from './rootStore';
 import { observable, action, runInAction, computed, reaction } from 'mobx';
-import { IProfile, IPhoto } from '../models/profile';
+import { IProfile, IPhoto, IUserActivity } from '../models/profile';
 import agent from '../api/agent';
 import { toast } from 'react-toastify';
 
@@ -27,8 +27,8 @@ export default class ProfileStore {
     @observable loading = false;
     @observable followings: IProfile[] = [];
     @observable activeTab: number = 0;
-  @observable userActivities: IUserActivity[] = [];
-  @observable loadingActivities = false;
+    @observable userActivities: IUserActivity[] = [];
+    @observable loadingActivities = false;
 
     @computed get isCurrentUser() {
         if (this.rootStore.userStore.user && this.profile) {
