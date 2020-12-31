c03d79d33e293fc09e597795c6c6a5da7d3a5155
Daniel <bahmani00@gmail.com>
2020-12-30 23:26:57 -0500

    fix Fallback issue

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -1,15 +1,30 @@
 using System.IO;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace API.Controllers
 {
     [AllowAnonymous]
     public class FallbackController : Controller
     {
+        // need hosting environment for base path
+        public IWebHostEnvironment HostingEnv { get; }
+
+        public FallbackController(IWebHostEnvironment env)
+        {
+            HostingEnv = env;
+        }    
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+            return new PhysicalFileResult(Path.Combine(HostingEnv.WebRootPath, "wwwroot","index.html"),
+                new MediaTypeHeaderValue("text/html") 
+            );
+
+            //return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
         }
     }
 }
